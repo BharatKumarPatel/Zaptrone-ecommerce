34bb2777bcbb5b39ca0dcbd510758b30af1bedc3
BharatKumarPatel <ibbchoudhary@gmail.com>
2025-04-04 22:16:51 +0530

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,25 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Home from './pages/Home';
+import Products from './pages/Products';
+import Cart from './pages/Cart';
+import Checkout from './pages/Checkout';
+
+function App() {
+  return (
+    <Router>
+      <div className="min-h-screen bg-gray-50">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/checkout" element={<Checkout />} />
+        </Routes>
+        <ToastContainer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
